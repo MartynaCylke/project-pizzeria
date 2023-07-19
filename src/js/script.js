8c50f05bd75137f9879bc715fa2204ace05031cd
Martyna <pugdogxxx@gmail.com>
2023-07-19 11:09:56 +0200

    modul7.5

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,27 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-  'use strict';
+  ("use strict");
 
-const select = {
-  templateOf: {
-    menuProduct: "#template-menu-product",
+  const select = {
+    templateOf: {
+      menuProduct: "#template-menu-product",
     },
     containerOf: {
-      menu: '#product-list',
-      cart: '#cart',
+      menu: "#product-list",
+      cart: "#cart",
     },
     all: {
-      menuProducts: '#product-list > .product',
-      menuProductsActive: '#product-list > .product.active',
-      formInputs: 'input, select',
+      menuProducts: "#product-list > .product",
+      menuProductsActive: "#product-list > .product.active",
+      formInputs: "input, select",
     },
     menuProduct: {
-      clickable: '.product__header',
-      form: '.product__order',
-      priceElem: '.product__total-price .price',
-      imageWrapper: '.product__images',
-      amountWidget: '.widget-amount',
+      clickable: ".product__header",
+      form: ".product__order",
+      priceElem: ".product__total-price .price",
+      imageWrapper: ".product__images",
+      amountWidget: ".widget-amount",
       cartButton: '[href="#add-to-cart"]',
     },
     widgets: {
@@ -35,8 +35,8 @@ const select = {
 
   const classNames = {
     menuProduct: {
-      wrapperActive: 'active',
-      imageVisible: 'active',
+      wrapperActive: "active",
+      imageVisible: "active",
     },
   };
 
@@ -45,21 +45,78 @@ const select = {
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    },
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    menuProduct: Handlebars.compile(
+      document.querySelector(select.templateOf.menuProduct).innerHTML
+    ),
   };
 
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      
+      thisProduct.renderInMenu();
+      console.log('new Product:', thisProduct);
+
+      thisProduct.initAccordion();
+    }
+  
+renderInMenu(){
+  const thisProduct = this;
+
+  initAccordion(){
+    const thisProduct = this;
+  }
+
+  /*generate HTML based on Template */
+  const generatedHTML = templates.menuProduct(thisProduct.data);
+
+  /* create element using utils.createElementFromHTML */
+  thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+  /* find menu container */
+  const menuContainer = document.querySelector(select.containerOf.menu);
+
+  /* add element to menu */
+  menuContainer.appendChild(thisProduct.element);
+}
+  }
+
+
+
+
+
+
+  initData: function(){
+    const thisApp = this;
+
+    thisApp.data = dataSource;
+  },
+
   const app = {
-    init: function(){
+    const thisApp = this;
+    console.log('thisApp.data:', thisApp.data);
+    
+    for (let productData in thisApp.data.products) {
+      new Product(productData, thisApp.data.products[productData]);
+    }
+
+    },
+    init: function () {
       const thisApp = this;
-      console.log('*** App starting ***');
-      console.log('thisApp:', thisApp);
-      console.log('classNames:', classNames);
-      console.log('settings:', settings);
-      console.log('templates:', templates);
+      console.log("*** App starting ***");
+      console.log("thisApp:", thisApp);
+      console.log("classNames:", classNames);
+      console.log("settings:", settings);
+      console.log("templates:", templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
