f07fa8592507f23d4bcde1ed49ef1d5ed8a85ff9
Martyna <pugdogxxx@gmail.com>
2023-08-11 16:12:26 +0200

    poprawki_ostatniego_zadania2

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,38 @@
 import { settings, select, classNames } from "./settings.js";
 import Product from "./components/Product.js";
 import Cart from "./components/Cart.js";
+
 const app = {
   initPages: function () {
     const thisApp = this;
+
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
     const idFromHash = window.location.hash.replace("#/", "");
+
     let pageMatchingHash = thisApp.pages[0].id;
+
     for (let page of thisApp.pages) {
       if (page.id == idFromHash) {
         pageMatchingHash = page.id;
         break;
       }
     }
+
     thisApp.activatePage(pageMatchingHash);
+
     for (let link of thisApp.navLinks) {
       link.addEventListener("click", function (event) {
         const clickedElement = this;
         event.preventDefault();
+
         /* get page id from href attribute */
         const id = clickedElement.getAttribute("href").replace("#", "");
+
         /* run thisApp.activatePage with that id */
         thisApp.activatePage(id);
+
         /* change URL hash */
         window.location.hash = "#" + id;
       });
@@ -30,10 +40,12 @@ const app = {
   },
   activatePage: function (pageId) {
     const thisApp = this;
+
     // add class "active" to matching pages, remove from non-matching
     for (let page of thisApp.pages) {
       page.classList.toggle(classNames.pages.active, page.id == pageId);
     }
+
     // add class "active" to matching links, remove from non-matching
     for (let link of thisApp.navLinks) {
       link.classList.toggle(
@@ -44,6 +56,7 @@ const app = {
   },
   initMenu: function () {
     const thisApp = this;
+
     // console.log('thisApp.data', thisApp.data);
     for (let productData in thisApp.data.products) {
       new Product(
@@ -74,9 +87,8 @@ const app = {
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
     thisApp.productList = document.querySelector(select.containerOf.menu);
-
     thisApp.productList.addEventListener("add-to-cart", function (event) {
-      app.cart.add(event.detail.product.prepareCartProduct());
+      app.cart.add(event.detail.product);
     });
   },
   init: function () {
@@ -86,7 +98,9 @@ const app = {
     // console.log('classNames:', classNames);
     // console.log('settings:', settings);
     // console.log('templates:', templates);
+
     thisApp.initPages();
+
     thisApp.initData();
     thisApp.initCart();
   },
